5033df5583332212e3e64d9a917a7aea3f698133
Erika Výchlopeňová <xvychlopenova@stuba.sk>
2022-05-18 17:29:28 +0200

    project setup

---
+++ config/config.php
@@ -0,0 +1,6 @@
+<?php
+
+$servername = "localhost";
+$username = "xvychlopenova";
+$password = "heslo";
+$dbname = "final";
---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="script" href="js/script.js">
+    <title>Tlmič automobilu</title>
+</head>
+<header><h1>Tlmič automobilu</h1></header>
+<body>
+
+
+
+<canvas id="animationCanvas"></canvas>
+
+</body>
+</html>
